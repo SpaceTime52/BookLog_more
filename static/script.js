a43749c0674df158c6b9702bcc618708ded27e88
Bohyeon Kim <love.free.work@gmail.com>
2022-07-21 00:35:13 +0900

    finish register word checker

--- static/script.js
+++ static/script.js
@@ -17,15 +17,15 @@
                               <!-- Nav of the page -->
                               <nav id="nav" class="nav justify-content-end">
                                 <ul class="nav">
-                                  <li class="nav-item">
-                                    <a class="nav-link text-dark" style="display: inline-block;" id="for_control_1" aria-current="page" href="javascript:open_loginModal();">로그인</a>
+                                  <li class="nav-item m-3">
+                                    <a class="nav-link text-dark m-3" style="display: inline-block;" id="for_control_1" aria-current="page" href="javascript:open_loginModal();">로그인</a>
                                   </li>
-                                  <li class="nav-item">
-                                    <a class="nav-link text-dark" style="display: inline-block;" id="for_control_2" href='javascript:open_resgisterModal();'>회원가입</a>
+                                  <li class="nav-item m-3">
+                                    <a class="nav-link text-dark m-3" style="display: inline-block;" id="for_control_2" href='javascript:open_resgisterModal();'>회원가입</a>
                                   </li>
-                                  <li class="nav-item">
-                                    <a class="nav-link text-dark disabled is-hidden" style="display: inline-block;" id="nickname"></a>
-                                    <a class="nav-link text-dark is-hidden" style="display: inline-block;" id="logout" onclick="click_logout()">로그아웃</a>
+                                  <li class="nav-item m-3">
+                                    <a class="nav-link text-dark disabled is-hidden m-3" style="display: inline-block;" id="nickname"></a>
+                                    <a class="nav-link text-dark is-hidden m-3" style="display: inline-block;" id="logout" onclick="click_logout()">로그아웃</a>
                                   </li>
                                 </ul>
                               </nav>
@@ -99,7 +99,8 @@ function render_loginRegisterModal() {
                             id="input-username-signup"
                             placeholder="아이디"
                           />
-                          <p id="help-id" class="help"> 아이디는 2-10자의 영문과 숫자와 일부 특수문자(._-)만 입력 가능합니다.</p>
+                          
+                          <p id="help-id" class="help"> 아이디는 6-10자의 영문과 숫자와 일부 특수문자(._-)만 입력 가능합니다.</p>
                           <p id="help-id-login" class="help is-danger"></p>
 
                           <input
@@ -124,7 +125,7 @@ function render_loginRegisterModal() {
                             id="input-nickname"
                             placeholder="닉네임"
                           />
-                          <p id="help-nickname" class="help"> 닉네임은 한글 2~4글자만 가능합니다. </p>
+                          <p id="help-nickname" class="help"> 닉네임은 한글 또는 영문 3~7글자만 가능합니다. </p>
                           
                         </div>
                       </div>
@@ -147,6 +148,123 @@ function open_loginModal() {
 
 function open_resgisterModal() {
   $("#registerModal").modal("show");
+
+  let idOnInput = document.getElementById("input-username-signup");
+  idOnInput.addEventListener("input", (event) => {
+    let username = event.target.value;
+
+    if (username == "") {
+      $("#help-id")
+        .text("아이디를 입력해주세요.")
+        .removeClass("is-safe")
+        .addClass("is-danger");
+    } else if (!iDchecker(username)) {
+      $("#help-id")
+        .text(
+          "아이디의 형식을 확인해주세요. 영문과 숫자, 일부 특수문자(._-) 사용 가능. 2-10자 길이"
+        )
+        .removeClass("is-safe")
+        .addClass("is-danger");
+    } else {
+      $("#help-id").addClass("is-loading");
+
+      $.ajax({
+        type: "POST",
+        url: "/register/check_dup",
+        data: {
+          username_give: username,
+        },
+        success: function (response) {
+          if (response["exists"]) {
+            $("#help-id")
+              .text("이미 존재하는 아이디입니다.")
+              .removeClass("is-safe")
+              .addClass("is-danger");
+          } else {
+            $("#help-id")
+              .text("사용할 수 있는 아이디입니다.")
+              .removeClass("is-danger")
+              .addClass("is-success");
+          }
+          $("#help-id").removeClass("is-loading");
+        },
+      });
+    }
+  });
+
+  let pw_1onInput = document.getElementById("input-password1");
+  pw_1onInput.addEventListener("input", (event) => {
+    let password = event.target.value;
+
+    console.log("pw1", password);
+
+    if (password == "") {
+      $("#help-password")
+        .text("비밀번호를 입력해주세요.")
+        .removeClass("is-safe")
+        .addClass("is-danger");
+    } else if (!pWchecker(password)) {
+      $("#help-password")
+        .text(
+          "비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자"
+        )
+        .removeClass("is-safe")
+        .addClass("is-danger");
+    } else {
+      $("#help-password")
+        .text("사용할 수 있는 비밀번호입니다.")
+        .removeClass("is-danger")
+        .addClass("is-success");
+    }
+  });
+
+  let pw_2onInput = document.getElementById("input-password2");
+  pw_2onInput.addEventListener("input", (event) => {
+    let password2 = event.target.value;
+    let password1 = document.getElementById("input-password1").value;
+
+    console.log("pw2", password2);
+    console.log("pw1", password1);
+
+    if (password2 == "") {
+      $("#help-password2")
+        .text("비밀번호를 입력해주세요.")
+        .removeClass("is-safe")
+        .addClass("is-danger");
+    } else if (password2 != password1) {
+      $("#help-password2")
+        .text("입력하신 두개의 비밀번호가 일치하지 않습니다.")
+        .removeClass("is-safe")
+        .addClass("is-danger");
+    } else {
+      $("#help-password2")
+        .text("비밀번호가 일치합니다.")
+        .removeClass("is-danger")
+        .addClass("is-success");
+    }
+  });
+
+  let nick_onInput = document.getElementById("input-nickname");
+  nick_onInput.addEventListener("input", (event) => {
+    let nickname = event.target.value;
+
+    if (nickname == "") {
+      $("help-nickname")
+        .text("리뷰를 작성하실 때 활용할 닉네임을 설정해주세요.")
+        .removeClass("is-safe")
+        .addClass("is-danger");
+    } else if (!nicknameChecker(nickname)) {
+      $("#help-nickname")
+        .text("닉네임의 형식을 확인해주세요.한글 또는 영문 3~7자 사용 가능")
+        .removeClass("is-safe")
+        .addClass("is-danger");
+    } else {
+      $("#help-nickname")
+        .text("닉네임으로 사용 가능합니다.")
+        .removeClass("is-danger")
+        .addClass("is-success");
+    }
+  });
 }
 
 // ------------------------------------------
@@ -209,163 +327,54 @@ function login_check(response) {
   });
 }
 
-function is_password(asValue) {
-  let regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$/;
-  return regExp.test(asValue);
-}
-
-function is_nick(asValue) {
-  let regExp = /^[가-힣]{2,4}$/;
-  return regExp.test(asValue);
-}
-
-function toggle_sign_up() {
-  window.location.href = "/register";
-}
-
 function sign_up() {
-  let username = $("#input-username").val();
-  let password = $("#input-password").val();
-  let password2 = $("#input-password2").val();
+  let username = $("#input-username-signupe").val();
+  let password = $("#input-password1").val();
   let nickname = $("#input-nickname").val();
 
-  if ($("#help-id").hasClass("is-danger")) {
-    alert("아이디를 다시 확인해주세요.");
-    return;
-  } else if (!$("#help-id").hasClass("is-success")) {
-    alert("아이디 중복확인을 해주세요.");
-    return;
-  }
+  if (
+    $("#help-id").hasClass("is-success") &&
+    $("#help-password").hasClass("is-success") &&
+    $("#help-password2").hasClass("is-success") &&
+    $("#help-nickname").hasClass("is-success")
+  ) {
+    $.ajax({
+      type: "POST",
+      url: "/register/sign_up",
+      data: {
+        username_give: username,
+        password_give: password,
+        nickname_give: nickname,
+      },
+      success: function (response) {
+        alert("회원가입을 축하드립니다!");
+        $("#registerModal").modal("hide");
 
-  if (password == "") {
-    $("#help-password")
-      .text("비밀번호를 입력해주세요.")
-      .removeClass("is-safe")
-      .addClass("is-danger");
-    $("#input-password").focus();
-    return;
-  } else if (!is_password(password)) {
-    $("#help-password")
-      .text(
-        "비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 특수문자(!@#$%^&*) 사용가능 8-20자"
-      )
-      .removeClass("is-safe")
-      .addClass("is-danger");
-    $("#input-password").focus();
-    return;
+        $("#input-username-signup").val()='';
+        $("#input-password1").empty();
+        $("#input-password2").empty();
+        $("#input-nickname").empty();
+      },
+    });
   } else {
-    $("#help-password")
-      .text("사용할 수 있는 비밀번호입니다.")
-      .removeClass("is-danger")
-      .addClass("is-success");
+    alert("기입하신 내용을 확인해주세요.");
   }
-  if (password2 == "") {
-    $("#help-password2")
-      .text("비밀번호를 입력해주세요.")
-      .removeClass("is-safe")
-      .addClass("is-danger");
-    $("#input-password2").focus();
-    return;
-  } else if (password2 != password) {
-    $("#help-password2")
-      .text("비밀번호가 일치하지 않습니다.")
-      .removeClass("is-safe")
-      .addClass("is-danger");
-    $("#input-password2").focus();
-    return;
-  } else {
-    $("#help-password2")
-      .text("비밀번호가 일치합니다.")
-      .removeClass("is-danger")
-      .addClass("is-success");
-  }
-  if (!is_nick(nickname)) {
-    $("#help-nickname")
-      .text("닉네임의 형식을 확인해주세요.한글 2~4자 사용 가능")
-      .removeClass("is-safe")
-      .addClass("is-danger");
-    $("#input-nickname").focus();
-    return;
-  }
-  $.ajax({
-    type: "POST",
-    url: "/register/sign_up",
-    data: {
-      username_give: username,
-      password_give: password,
-      nickname_give: nickname,
-    },
-    success: function (response) {
-      alert("회원가입을 축하드립니다!");
-      window.location.replace("/login");
-    },
-  });
 }
 
 // 정규표현식 검증 함수
-function is_nickname(asValue) {
-  let regExp = /^(?=.*[a-zA-Z])[-a-zA-Z0-9_.]{2,10}$/;
-  return regExp.test(asValue);
+function iDchecker(username) {
+  let regExp = /^(?=.*[a-zA-Z])[-a-zA-Z0-9_.]{6,10}$/;
+  return regExp.test(username);
 }
 
-function is_password(asValue) {
+function pWchecker(pw) {
   let regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$/;
-  return regExp.test(asValue);
-}
-function is_nick(asValue) {
-  let regExp = /^[가-힣]{2,4}$/;
-  return regExp.test(asValue);
+  return regExp.test(pw);
 }
 
-//아이디 중복확인 함수
-function check_dup() {
-  let username = $("#input-username").val();
-
-  if (username == "") {
-    $("#help-id")
-      .text("아이디를 입력해주세요.")
-      .removeClass("is-safe")
-      .addClass("is-danger");
-    $("#input-username").focus();
-    return;
-  }
-  if (!is_nickname(username)) {
-    $("#help-id")
-      .text(
-        "아이디의 형식을 확인해주세요. 영문과 숫자, 일부 특수문자(._-) 사용 가능. 2-10자 길이"
-      )
-      .removeClass("is-safe")
-      .addClass("is-danger");
-    $("#input-username").focus();
-    return;
-  }
-
-  $("#help-id").addClass("is-loading");
-  $.ajax({
-    type: "POST",
-    url: "/register/check_dup",
-    data: {
-      username_give: username,
-    },
-    success: function (response) {
-      if (response["exists"]) {
-        $("#help-id")
-          .text("이미 존재하는 아이디입니다.")
-          .removeClass("is-safe")
-          .addClass("is-danger");
-        $("#input-username").focus();
-      } else {
-        $("#help-id")
-          .text("사용할 수 있는 아이디입니다.")
-          .removeClass("is-danger")
-          .addClass("is-success");
-      }
-      $("#help-id").removeClass("is-loading");
-    },
-  });
-}
-function toggle_sign_up() {
-  window.location.replace("/");
+function nicknameChecker(nickname) {
+  let regExp = /^[가-힣0-9a-zA-Z]{3,7}$/;
+  return regExp.test(nickname);
 }
 
 // (function (factory) {
